925f9705953ecbaf4ff0d6d8b23fb70c3d5addb6
Tacəddin Əsədzadə <tcddnesedzade@fmail.com>
2024-10-03 09:27:27 +0400

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int deciml, simple;
+int num = Convert.ToInt32(Console.ReadLine());
+deciml = num / 10;
+simple = num % 10;
+if (deciml > simple)
+{
+    Console.WriteLine(deciml);
+}
+if (simple > deciml)
+{
+    Console.WriteLine(simple);
+}
